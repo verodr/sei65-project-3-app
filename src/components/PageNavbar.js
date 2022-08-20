a5303d9967e1136a773ef9ab59afcdec9cc91892
dr-veronica <veronica.deronzi@gmail.com>
2022-08-20 21:00:14 +0100

    creating comment page and correcting error in Topic Page

--- src/components/PageNavbar.js
+++ src/components/PageNavbar.js
@@ -11,7 +11,7 @@ const PageNavbar = () => {
         <Navbar.Toggle aria-controls='basic-navbar-nav' />
         <Navbar.Collapse id="basic-navbar-nav" className='justify-content-end'>
           <Nav.Link as={Link} to="/">HOME</Nav.Link>
-          <Nav.Link as={Link} to="/topic">ALL TOPIC</Nav.Link>
+          <Nav.Link as={Link} to="/topic">ALL TOPICS</Nav.Link>
           <Nav.Link as={Link} to="/login">LOGIN</Nav.Link>
           <Nav.Link as={Link} to="/register">REGISTER</Nav.Link>
         </Navbar.Collapse>
