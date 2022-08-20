2e59be1da6471732d332b0741f6cd0c9f617dd99
Mark Muyuela <mark.muyuela81@gmail.com>
2022-08-20 10:46:46 +0100

    topicpage info being pulled in

--- src/components/TopicPage.js
+++ src/components/TopicPage.js
@@ -1,7 +1,59 @@
+// Import Hooks
+import { useEffect, useState } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+
+// Import Bootstrap Components
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Card from 'react-bootstrap/Card'
 
 const TopicPage = () => {
+  const [ topic, setTopic ] = useState([])
+  const [ errors, setErrors] = useState(false)
+
+
+  // ! Executed
+  useEffect(() => {
+  // Get our bread data
+    const getData = async () => {
+      try {
+        const { data } = await axios.get('http://localhost:4000/topic')
+        // console.log(response)
+        setTopic(data)
+        console.log('data', data )
+      } catch (err) {
+        console.log(err)
+        setErrors(true)
+      }
+    }
+    getData()
+  }, [])
+
+
   return (
-    <h1>TopicPage</h1>
+    <Container as="main">
+      <h1 className='text-center mb-4'>topic</h1>
+      <Row>
+        { topic.map(titles => {
+          const { _id, topic, imageUrl } = titles
+          console.log('topic', topic)
+          return (
+            <Col key={_id} md="6" lg="4" className='mb-4'>
+              <Link to={`/bread/${_id}`}>
+                <Card>
+                  <Card.Img variant='top' src={imageUrl}></Card.Img>
+                  <Card.Body className='bg-light'>
+                    <Card.Title className='text-center mb-0'>{topic} - {}</Card.Title>
+                  </Card.Body>
+                </Card>
+              </Link>
+            </Col>
+          )
+        }) }
+      </Row>
+    </Container>
   )
 
 }
