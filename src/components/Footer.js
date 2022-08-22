884ddc7fabed5e3562d48992caa171b2e373ee9e
Mark Muyuela <mark.muyuela81@gmail.com>
2022-08-22 15:05:27 +0100

    footer updated - LI logo

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom'
-import image from '../styles/images/LI-In-Bug.png'
+import image from '../styles/images/LinkedIn-logo.png'
 import Container from 'react-bootstrap/Container'
 import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
@@ -13,9 +13,11 @@ const Footer = () => {
       
       <div className="footer-container"> 
         <div className="Mark" href="https://github.com/markmuy40" target="_blank" rel="noreferrer">Mark Muyuela
-          <div className="Logo" href="https://www.linkedin.com/in/mark-muyuela/" target="_blank" rel="noreferrer" src={image}></div>
+          <div className="Logo"><a href="https://www.linkedin.com/in/mark-muyuela/" target="_blank" rel="noreferrer"><img src={image} height="50px"></img></a></div>
         </div>
-        <div className="Rokas" href="https://github.com/rokster112" target="_blank" rel="noreferrer">Rokas Arlauskas</div>
+        <div className="Rokas" href="https://github.com/rokster112" target="_blank" rel="noreferrer">Rokas Arlauskas
+          <div className="Logo"><a href="https://www.linkedin.com/in/mark-muyuela/" target="_blank" rel="noreferrer"><img src={image} height="50px"></img></a></div>
+        </div>
         <div className="Veronica" href="https://github.com/verodr" target="_blank" rel="noreferrer">Veronica DeRonzi</div>
       </div>
     </div>  
